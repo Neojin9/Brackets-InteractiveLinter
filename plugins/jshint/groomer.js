55971b762d8c8669b5338055a01d87467b0a784e
Neojin9 <neojin9@gmail.com>
2014-05-31 13:18:34 -0400

    Update groomer.js
    Add codeHref property to message object to create clickable link when message is displayed

--- plugins/jshint/groomer.js
+++ plugins/jshint/groomer.js
@@ -216,7 +216,7 @@ define(function (require, exports, module) {
 
         // Add href for help
         message.href = "http://jslinterrors.com/" + (message.raw || "").replace(/'*\{*(\w*)\}*'*/g, "$1").replace(/\s/g, '-').replace(/\.$/, '').toLowerCase();
-        message.reason += " - " + message.code;
+        message.codeHref = "http://jslinterrors.com/" + (message.code || "");
 
         //console.log(token, message);
         return token;
